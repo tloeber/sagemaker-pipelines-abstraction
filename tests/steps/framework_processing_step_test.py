39d6dcd20dc3f75dc5ab787a933201200490d259
tloeber <thomas.loeber73@gmail.com>
2024-03-04 10:00:49 -0500

    Update test to use s3path obj; add testing of muliple inputs and outputs.

--- tests/steps/framework_processing_step_test.py
+++ tests/steps/framework_processing_step_test.py
@@ -1,12 +1,12 @@
 import pytest
 from typing import Any
-from pathlib import Path
+from s3path import S3Path
 
 from sagemaker.processing import ProcessingInput, ProcessingOutput
 from sagemaker.workflow.pipeline_context import PipelineSession, LocalPipelineSession
 
 from sm_pipelines_oo.steps.framework_processing_step import (
-    FrameworkProcessingStepFactory, FrameworkProcessorRunArgs, _FWProcessorRunConfig
+    StepFactory, RunArgs, _RunConfig
 )
 
 
@@ -18,10 +18,10 @@ from sm_pipelines_oo.steps.framework_processing_step import (
 run_args_config_dict_1: dict[str, Any] = {
     'code': 'code.py',
     'source_dir': 'code_dir/',
-    'input_files_s3paths': [Path('s3://test_bucket/input/input1.txt')],
-    'output_files_s3paths': [Path('s3://test_bucket/output/output1.txt')],
+    'input_files_s3paths': ['s3://test_bucket/input/input1.txt'],
+    'output_files_s3paths': ['s3://test_bucket/output/output1.txt'],
 }
-expected_run_args_1: FrameworkProcessorRunArgs = {
+expected_run_args_1: RunArgs = {
     'code': 'code.py',
     'source_dir': 'code_dir/',
     'inputs': [
@@ -34,7 +34,8 @@ expected_run_args_1: FrameworkProcessorRunArgs = {
     'outputs': [
         ProcessingOutput(
             output_name='output1',
-            source='s3://test_bucket/output/output1.txt'
+            source='/opt/ml/processing/output1.txt',
+            destination='s3://test_bucket/output/output1.txt',
         )
     ]
 }
@@ -45,30 +46,43 @@ run_args_config_dict_2: dict[str, Any] = {
     'code': 'code.py',
     'source_dir': 'code_dir/',
     'input_files_s3paths': [
-        Path('s3://test_bucket/input/input1.txt'),
-        Path('s3://test_bucket/input/input2.txt'),
-        Path('s3://test_bucket/input/input3.txt'),
+        's3://test_bucket/input/input1.txt',
+        's3://test_bucket/input/input2.txt',
     ],
     'output_files_s3paths': [
-        Path('s3://test_bucket/output/output2.txt')
+        's3://test_bucket/output/output1.txt',
+        's3://test_bucket/output/output2.txt'
     ]
 }
 
-expected_run_args_2: FrameworkProcessorRunArgs = {
+expected_run_args_2: RunArgs = {
     'code': 'code.py',
     'source_dir': 'code_dir/',
     'inputs': [
         ProcessingInput(
             input_name='input1',
+            source='s3://test_bucket/input/input1.txt',
+            destination='/opt/ml/processing/input1.txt'
+        ),
+        ProcessingInput(
+            input_name='input2',
             source='s3://test_bucket/input/input2.txt',
-            destination='/opt/ml/type1/step1/input2.txt'
+            destination='/opt/ml/processing/input2.txt'
         )
+
     ],
     'outputs': [
         ProcessingOutput(
             output_name='output1',
-            source='s3://test_bucket/output/output2.txt'
+            source='/opt/ml/processing/output1.txt',
+            destination='s3://test_bucket/output/output1.txt'
+        ),
+        ProcessingOutput(
+            output_name='output2',
+            source='/opt/ml/processing/output2.txt',
+            destination='s3://test_bucket/output/output2.txt'
         )
+
     ]
 }
 
@@ -80,7 +94,7 @@ expected_run_args_2: FrameworkProcessorRunArgs = {
     argnames="run_config_dict, expected_run_args",
     argvalues=[
         (run_args_config_dict_1, expected_run_args_1),
-        # (run_args_config_dict_2, expected_run_args_2),
+        (run_args_config_dict_2, expected_run_args_2),
     ]
 )
 def test_construct_processing_input_output(run_config_dict: dict[str, Any], expected_run_args):
@@ -104,11 +118,36 @@ def test_construct_processing_input_output(run_config_dict: dict[str, Any], expe
             'role_name': 'test_role'
         }
     }
-    framework_processing_step_factory = FrameworkProcessingStepFactory(
+    framework_processing_step_factory = StepFactory(
         step_config_dict=step_config_dict,
         role_arn='mock-role-arn',
         pipeline_session=LocalPipelineSession()
     )
-    actual_run_args: FrameworkProcessorRunArgs = \
+    actual_run_args: RunArgs = \
         framework_processing_step_factory._construct_run_args()
-    assert actual_run_args['inputs'][0].input_name == expected_run_args['inputs'][0].input_name
+
+    # Compare
+    # =======
+    # Note: Have to compare fields because ProcessingInput and ProcessingOutput don't have a string representation implemented. While hash is implemented, it is different even though fields are the same.
+    # Note: unfortunately it's also not possible to specify the fields to compare as a list, and then iterate through it. The reason is that the processing input can't be converted to a dictionary, so we have to stick with dot notation for attribute access.
+
+    # inputs
+    # ------
+    actual_inputs: list[ProcessingInput] = actual_run_args['inputs']
+    expected_inputs: list[ProcessingInput] = expected_run_args['inputs']
+    # Go through *list* of inputs, and for each input compare the fields
+    for actual_input, expected_input in zip(actual_inputs, expected_inputs):
+        assert actual_input.input_name == expected_input.input_name
+        assert actual_input.source == expected_input.source
+        assert actual_input.destination == expected_input.destination
+
+    # outputs
+    # -------
+    # Note: Didn't combine this logic with input checking for now, but we may want to compare different fields for each in the future.
+    actual_outputs: list[ProcessingOutput] = actual_run_args['outputs']
+    expected_outputs: list[ProcessingOutput] = expected_run_args['outputs']
+    # Go through *list* of outputs, and for each output compare the fields
+    for actual_output, expected_output in zip(actual_outputs, expected_outputs):
+        assert actual_output.output_name == expected_output.output_name
+        assert actual_output.source == expected_output.source
+        assert actual_output.destination == expected_output.destination

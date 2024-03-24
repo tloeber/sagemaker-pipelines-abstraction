775b7314e2c16f7b8d032261e344f72d815139e5
tloeber <thomas.loeber73@gmail.com>
2024-03-24 11:25:04 -0400

    Add a decorator to validation that excludes timestamp column from transform and just passes it through

--- examples/src/smp_oo_examples/worker_code/preprocess/preprocess.py
+++ examples/src/smp_oo_examples/worker_code/preprocess/preprocess.py
@@ -3,6 +3,14 @@ import pandas as pd
 # from loguru import logger
 
 
+try:
+    # This is how it will be imported on worker, where smp_oo_examples is NOT installed as a library
+    from validation import transform_except_date
+except ModuleNotFoundError:
+    # For running tests, etc.
+    from smp_oo_examples.worker_code.preprocess.validation import transform_except_date
+
+@transform_except_date
 def transform(df: pd.DataFrame) -> pd.DataFrame:
     """Double all values in DataFrame and add current date."""
     return df \
